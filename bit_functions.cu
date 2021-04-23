bf2b9212109b7d5c2735d4d82c12fa84fdb11a11
daveedoo <damaks48@gmail.com>
2021-04-23 17:54:39 +0200

    .

--- bit_functions.cu
+++ bit_functions.cu
@@ -1,41 +1,33 @@
 #include "bit_functions.cuh"
 
-//gets i-th bit in int
 __device__ UINT get_bit(UINT src, int i)
 {
 	return (1 & (src >> (31 - (i))));
 }
 
-//sets u-th bit to 1
 __device__ UINT fill_bit(UINT src, int i)
 {
 	src |= 1UL << (31 - i);
 	return src;
 }
 
-//sets u-th bit to 0
 __device__ UINT clear_bit(UINT src, int i)
 {
 	src &= ~(1UL << (31 - i));
 	return src;
 }
 
-//checks if all bits in src are zero
 __device__ bool is_zeros(UINT src)
 {
 	return src == 0;
 }
-//checks if all bits in src are ones
+
 __device__ bool is_ones(UINT src)
 {
 	src = fill_bit(src, 0);
 	return (~src) == 0;
 }
 
-//Compresses n blocks of 'bit'
-//1st bit - is compressed flag
-//2nd bit - what bit is in series (all 0 or all 1)
-//3rd to end - block count
 __device__ UINT get_compressed(UINT n, int bit)
 {
 	UINT rs = n;
--- bit_functions.cuh
+++ bit_functions.cuh
@@ -1,16 +1,25 @@
 #include "device_launch_parameters.h"
 #include "defines.h"
 
+//gets i-th bit in int
 __device__ UINT get_bit(UINT src, int i);
 
+//sets u-th bit to 1
 __device__ UINT fill_bit(UINT src, int i);
 
+//sets u-th bit to 0
 __device__ UINT clear_bit(UINT src, int i);
 
+//checks if all bits in src are zero
 __device__ bool is_zeros(UINT src);
 
+//checks if all bits in src are ones
 __device__ bool is_ones(UINT src);
 
+//Compresses n blocks of 'bit'
+//1st bit - is compressed flag
+//2nd bit - what bit is in series (all 0 or all 1)
+//3rd to end - block count
 __device__ UINT get_compressed(UINT n, int bit);
 
 __device__ UINT reverse(UINT src);
