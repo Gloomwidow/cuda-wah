21131934172e3f059431616eb962205b8ab4a0b2
Gloomwidow <michalmachnio.mm@gmail.com>
2021-07-09 12:11:17 +0200

    Optimized file loading method

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@
 #include "bit_functions.cuh"
 
 
-void CharTextBenchmark();
+void CharTextBenchmark(long int batch_char_size, std::string data_filename);
 typedef std::chrono::high_resolution_clock Time;
 typedef std::chrono::duration<float> fsec;
 
@@ -25,9 +25,9 @@ int main() {
    
     if (UNIT_TESTING)
     {
-        //UnitTests(&BallotSyncWAH);
-        //UnitTests(&AtomicAddWAH);
-		UnitTests(&SharedMemWAH);
+        UnitTests(&BallotSyncWAH);
+        UnitTests(&AtomicAddWAH);
+		//UnitTests(&SharedMemWAH);
 		//UnitTests(&RemoveIfSharedMemWAH);
     }
     else
@@ -35,7 +35,7 @@ int main() {
         printf("Launching Warm Up Kernel...\n");
         WarmUp();
         printf("Starting tests data: ASCII texts\n");
-        CharTextBenchmark();
+        CharTextBenchmark(50000000,"database.txt");
     }
     return 0;
 }
@@ -50,10 +50,9 @@ void RunWithBatch(int batch_reserve, int batch_pos, int batch_size, std::string 
     cudaFree(d_data);
 }
 
-void CharTextBenchmark()
+void CharTextBenchmark(long int batch_char_size, std::string data_filename)
 {
 
-    std::string data_filename = "database.txt";
     std::ofstream log("results_load.csv", std::ios_base::app | std::ios_base::out);
     std::fstream file("Data/" + data_filename, std::ios::binary | std::ios::in);
     if (!file)
@@ -62,123 +61,77 @@ void CharTextBenchmark()
         return;
     }
 
-    
-    
-    //printf("%lld\n", file_size);
+    long int batch_bit_size = batch_char_size * 8;
+    long int batch_int_count = (long int)ceil((batch_bit_size*1.0f) / 31.0f);
 
-    long int batch_char_size = 5000;
-    
+    UINT* data = new UINT[batch_int_count];
 
-
-    int batch_pos = 0;
-    int curr_batch_size = 0;
     unsigned long long bit_pos = 0;
-
-    std::vector<int> bits;
-    int local_bit = 0;
-    
+    int curr_char = 0;
+    int batch_part_index = 0;
     auto start = Time::now();
     while (!file.eof())
     {
-        if (curr_batch_size < batch_char_size)
+        // fill table with data from file
+        while (curr_char < batch_char_size) 
         {
             char c;
             file.get(c);
             for (int i = 7; i >= 0; i--)
             {
-                if (local_bit == 0)
+                int uint_pos = bit_pos / 32;             
+                if (bit_pos % 32 == 0) //first bit in UINT (WAH flag bit) should be 0 for algorithms 
                 {
-                    bits.push_back(0);
-                    local_bit++;
+                    data[uint_pos] = clear_bit(data[uint_pos], 0);
+                    bit_pos++;
                 }
-                int bit = ((c >> i) & 1);
-                //printf("%d", bit);
-                bits.push_back(bit);
-                local_bit = (local_bit + 1) % 32;
+                int curr_bit = ((c >> i) & 1);
+                int local_uint_pos = bit_pos % 32;
+                if(curr_bit) data[uint_pos] = fill_bit(data[uint_pos], local_uint_pos);
+                else data[uint_pos] = clear_bit(data[uint_pos], local_uint_pos);
+                bit_pos++;
             }
-            //printf("\n");
-            curr_batch_size++;
+            curr_char++;
         }
-        else
+        //fill remaining places with 0 bits
+        if (bit_pos < batch_bit_size)
         {
-            int rem = bits.size() % 32;
-            int to_fill = rem!=0?32-rem:0;
-            //printf("%d, %d, to fill: %d\n",bits.size(), bits.size()/32, to_fill);
-            for (int i = 0; i < to_fill; i++)
-            {
-                bits.push_back(0);
-            }
-            int data_size = bits.size() / 32;
-            UINT* data = new UINT[data_size];
-            
-            for (int i = 0; i < data_size; i++)
-            {
-                for (int j = 0; j < 32; j++)
-                {
-                    if (bits[i*32+j]) data[i] = fill_bit(data[i], j);
-                    else data[i] = clear_bit(data[i], j);
-                }
-            }
-            auto end = Time::now();
-            fsec fs = end - start; 
-            log << data_filename + ";" + std::to_string(batch_pos) + ";" + std::to_string(batch_char_size * 8) + ";" + std::to_string(fs.count()) + ";"<<std::endl;
-            printf("Batch %d load done...\n", batch_pos + 1);
-            RunWithBatch(bits.size() / 32, batch_pos, batch_char_size * 8, data_filename, data);
-            printf("Batch %d CUDA done...\n", batch_pos + 1);
-            bits.clear();         
-            batch_pos++;
-            curr_batch_size = 0;
-            delete[] data;
-            start = Time::now();
-        }
-    }   
-    if (curr_batch_size < batch_char_size && curr_batch_size>0)
-    {
-        while (curr_batch_size < batch_char_size)
-
-        {
-            char c = 0;
-            for (int i = 7; i >= 0; i--)
-            {
-                if (local_bit == 0)
-                {
-                    bits.push_back(0);
-                    local_bit++;
-                }
-                int bit = ((c >> i) & 1);
-                bits.push_back(bit);
-                local_bit = (local_bit + 1) % 32;
-            }
-            //printf("\n");
-            curr_batch_size++;
-        }
-        int rem = bits.size() % 32;
-        int to_fill = rem != 0 ? 32 - rem : 0;
-        //printf("%d, %d, to fill: %d\n",bits.size(), bits.size()/32, to_fill);
-        for (int i = 0; i < to_fill; i++)
-        {
-            bits.push_back(0);
-        }
-        int data_size = bits.size() / 32;
-        UINT* data = new UINT[data_size];
-
-        for (int i = 0; i < data_size; i++)
-        {
-            for (int j = 0; j < 32; j++)
-            {
-                if (bits[i * 32 + j]) data[i] = fill_bit(data[i], j);
-                else data[i] = clear_bit(data[i], j);
-            }
-        }
+            int uint_pos = bit_pos / 32;
+            int local_uint_pos = bit_pos % 32;
+            data[uint_pos] = clear_bit(data[uint_pos], local_uint_pos);
+            bit_pos++;
+        } 
         auto end = Time::now();
-        fsec fs = end - start;
-        log << data_filename + ";"+std::to_string(batch_pos)+";"+ std::to_string(batch_char_size * 8) + ";" + std::to_string(fs.count()) + ";" << std::endl;
-        RunWithBatch(bits.size() / 32, batch_pos, batch_char_size * 8, data_filename, data);
-        bits.clear();
-        batch_pos++;
-        curr_batch_size = 0;
-        delete[] data;
+        fsec fs = end - start; 
+        log << data_filename + ";" + std::to_string(batch_part_index) + ";" + std::to_string(batch_bit_size) + ";" + std::to_string(fs.count()) + ";"<<std::endl;
+        printf("Batch %d load done...\n", batch_part_index + 1);
+        RunWithBatch(batch_int_count, batch_part_index, batch_bit_size, data_filename, data);
+        printf("Batch %d CUDA done...\n", batch_part_index + 1);
+        batch_part_index++;
+        curr_char = 0;
+        bit_pos = 0;
+        start = Time::now();
     }
+    //fill remaining places with 0 if no file data is left
+    if (bit_pos < batch_bit_size)
+    {
+        int uint_pos = bit_pos / 32;
+        int local_uint_pos = bit_pos % 32;
+        data[uint_pos] = clear_bit(data[uint_pos], local_uint_pos);
+        bit_pos++;
+    }
+    auto end = Time::now();
+    fsec fs = end - start;
+    log << data_filename + ";" + std::to_string(batch_part_index) + ";" + std::to_string(batch_bit_size) + ";" + std::to_string(fs.count()) + ";" << std::endl;
+    printf("Batch %d load done...\n", batch_part_index + 1);
+    RunWithBatch(batch_int_count, batch_part_index, batch_bit_size, data_filename, data);
+    printf("Batch %d CUDA done...\n", batch_part_index + 1);
+    batch_part_index++;
+    curr_char = 0;
+    bit_pos = 0;
+    start = Time::now();
+
     file.close();
     log.close();
+    delete[] data;
 }
